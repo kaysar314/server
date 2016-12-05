03d50749778e23af848379b18b01c414538b2684
海萨尔·阿斯哈尔 <hse0724@175game.com>
2016-12-05 16:20:52 +0800

    add sozdik

---
+++ sozdik/server.js
@@ -0,0 +1,17 @@
+var httpd = require('http').createServer(handler);
+var io = require('socket.io').listen(httpd);
+var fs = require('fs');
+httpd.listen(80);
+
+function handler(req,res) {
+    fs.readFile(__dirname+'/static/'+(req.url==='/'?'index.html':req.url),
+        function (err,data) {
+            if(err){
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        }
+    );
+}
